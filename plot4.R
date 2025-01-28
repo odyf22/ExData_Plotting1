de2f109caa96d9fcf9e34a16ec8b583437bf1eee
odyf22 <ody.fokas@gmail.com>
2025-01-28 14:16:43 +0200

    Updated plot4.R and plot4.png to match the desired output

--- plot4.R
+++ plot4.R
@@ -22,13 +22,33 @@ subset_data$Datetime <- as.POSIXct(paste(subset_data$Date, subset_data$Time),
 
 
 
+# Load data (assuming it's already filtered and preprocessed as 'subset_data')
+# subset_data is a dataframe containing columns 'Datetime', 'Global_active_power',
+# 'Voltage', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3', and 'Global_reactive_power'.
 
-
-
-# Plot 4: Global Reactive Power Over Time
+# Set up a 2x2 plotting area
 png("plot4.png", width = 480, height = 480)
-plot(subset_data$Datetime, subset_data$Global_reactive_power, type = "l", 
-     xlab = "Datetime", ylab = "Global Reactive Power (kilowatts)", 
-     main = "Global Reactive Power Over Time")
-dev.off()
+par(mfrow = c(2, 2))  # Create a 2x2 grid of plots
 
+# Plot 1: Global Active Power
+plot(subset_data$Datetime, subset_data$Global_active_power, type = "l", 
+     xlab = "", ylab = "Global Active Power", main = "")
+
+# Plot 2: Voltage
+plot(subset_data$Datetime, subset_data$Voltage, type = "l", 
+     xlab = "datetime", ylab = "Voltage", main = "")
+
+# Plot 3: Energy Sub Metering
+plot(subset_data$Datetime, subset_data$Sub_metering_1, type = "l", 
+     xlab = "", ylab = "Energy sub metering")
+lines(subset_data$Datetime, subset_data$Sub_metering_2, col = "red")
+lines(subset_data$Datetime, subset_data$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = 1, bty = "n")
+
+# Plot 4: Global Reactive Power
+plot(subset_data$Datetime, subset_data$Global_reactive_power, type = "l", 
+     xlab = "datetime", ylab = "Global Reactive Power", main = "")
+
+# Close the plotting device
+dev.off()
